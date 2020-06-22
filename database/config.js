1e28d97bb087244ca42531f77881c97be8cabaa6
daief ullah khan <sardarbhai6@gmail.com>
2020-06-23 00:26:36 +0530

    deploy

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 async function connectDB(){
-const url = "mongodb://localhost:27017/newsapi";
+const url = "mongodb+srv://abdullah:abdullah420@cluster0-wli1l.mongodb.net/NewsDatabase?retryWrites=true&w=majority";
 await mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
   console.log(err ? "error" : "connected");
 });
