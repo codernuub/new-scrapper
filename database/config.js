c77675e2b0a20ab7d8bd71f5bc601605e4d51e1e
Abdullah <42990725+codernuub@users.noreply.github.com>
2020-06-23 12:04:29 +0530

    Removed url

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 async function connectDB(){
-const url = "mongodb+srv://abdullah:abdullah420@cluster0-wli1l.mongodb.net/NewsDatabase?retryWrites=true&w=majority";
+const url = "";
 await mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
   console.log(err ? "error" : "connected");
 });
